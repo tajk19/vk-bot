cccfe94d735e20383370b622b2ecde3ada72d1f8
tajk19 <ramazan.troshko22@gmail.com>
2025-11-17 22:08:47 +0300

    fix: small fix date format for notifications

--- notifications.py
+++ notifications.py
@@ -49,7 +49,7 @@ async def notification_loop(bot: Bot):
             booking_time_str = f"{booking['Дата']} {booking['Время']}"
             try:
                 booking_start = datetime.strptime(
-                    booking_time_str, "%Y-%m-%d %H:%M"
+                    booking_time_str, "%d-%m-%y %H:%M"
                 )
                 booking_start = booking_start.replace(tzinfo=moscow_tz)
             except ValueError:
